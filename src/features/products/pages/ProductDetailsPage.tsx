e3b3e8f2d0f8053dba86e564ce3c9dfc604607a2
Ritik <ritik@logiqlink.com>
2026-06-10 00:06:15 +0530

    implement product details page and create shared back button component

--- src/features/products/pages/ProductDetailsPage.tsx
+++ src/features/products/pages/ProductDetailsPage.tsx
@@ -1,8 +1,56 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import type { Product } from "../types";
+import { getProductById } from "../api/productApi";
+import BackButton from "../../../shared/components/BackButton";
 
 function ProductDetailsPage() {
+  const { id } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        setLoading(true);
+        const data = await getProductById(id!);
+        setProduct(data);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProduct();
+  }, [id]);
+
+  if (loading) return <h2>Loading...</h2>;
+  if (!product) return <h2>Product not found</h2>;
+
   return (
-    <div>ProductDetailsPage</div>
-  )
+    <div className="">
+      <BackButton />
+      <img
+        src={product.images[0]}
+        alt={product.title}
+        className="w-full max-h-[500px] object-cover"
+      />
+
+      <h1 className="text-3xl font-bold mt-4">
+        {product.title}
+      </h1>
+
+      <p className="mt-4">{product.description}</p>
+
+      <h2 className="text-2xl font-bold mt-4">
+        ${product.price}
+      </h2>
+
+      <button className="mt-6 px-4 py-2 border rounded">
+        Add To Cart
+      </button>
+    </div>
+  );
 }
 
-export default ProductDetailsPage
+export default ProductDetailsPage;
---
+++ src/shared/components/BackButton.tsx
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
+function BackButton() {
+    const navigate = useNavigate();
+    return (
+        <button
+            onClick={() => navigate(-1)}
+            className="mb-4 px-4 py-2 border rounded"
+        >
+            Back
+        </button>
+    );
+}
+
+export default BackButton
